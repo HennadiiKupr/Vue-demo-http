e561b0df9d87b67669d08fc7d3f3ff1cebb7fbf6
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2024-07-21 02:03:59 +0300

    added loading message and no data message

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,9 +5,15 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
-        <survey-result v-for="result in results" :key="result.id" :name="result.name"
-          :rating="result.rating"></survey-result>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && (!results || results.lenght === 0)">No stored experiences found. Start adding some survey results first.</p>
+      <ul v-else-if="!isLoading && results.lenght > 0">
+        <survey-result 
+          v-for="result in results" 
+          :key="result.id" 
+          :name="result.name"
+          :rating="result.rating">
+        </survey-result>
       </ul>
     </base-card>
   </section>
@@ -23,15 +29,18 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     }
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vue-http-demo-dcda7-default-rtdb.firebaseio.com/surveys.json').then((response) => {
         if (response.ok) {
           return response.json();
         }
       }).then((data) => {
+        this.isLoading = false;
         const results = [];
         for (const id in data) {
           results.push({ 
